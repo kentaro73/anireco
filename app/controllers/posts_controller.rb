ebd0ea18db04290a145b563e835e8cef2a9fae6c
kentaro73 <k.uneune0703@gmail.com>
2020-08-24 08:34:51 +0900

    delete test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,11 @@ class PostsController < ApplicationController
     @posts = Post.all.order(created_at: :desc)
   end
 
+
+  def test
+    @posts = Post.all.order(created_at: :desc)
+  end
+
   def show
   end
 
