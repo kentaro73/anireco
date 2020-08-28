3d7702dccd10ef7db060390d2e1ac032e9cfcd62
kentaro73 <k.uneune0703@gmail.com>
2020-08-28 21:32:43 +0900

    add comment destroy and change pagination style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :correct_user, only: [:destroy]
+  
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
@@ -13,9 +14,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment.destroy
+    flash[:notice] = "コメントを削除しました"
+    redirect_back(fallback_location: root_path)
+  end
+
   private
 
     def comment_params
       params.require(:comment).permit(:content)
+    end
+
+    def correct_user
+      @comment = current_user.comments.find_by(id: params[:id])
+      unless @comment
+        redirect_to root_url
+      end
     end
 end
