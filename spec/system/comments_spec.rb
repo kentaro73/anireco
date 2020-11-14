c46fbcd0bf487d6d0a14ad2eb475f93d89332620
kentaro73 <k.uneune0703@gmail.com>
2020-11-14 22:03:56 +0900

    translate comments_spec.rb to English from Japanese

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "Comments", type: :system do
       end
 
       it "コメントボタンがないこと" do
-        expect(page).to have_no_button "コメントする"
+        expect(page).to have_no_button "Comment"
       end
 
       it "コメントが表示されていること" do
@@ -29,36 +29,36 @@ RSpec.describe "Comments", type: :system do
       end
 
       it "削除リンクが出ていないこと" do
-        expect(page).to have_no_link "削除"
+        expect(page).to have_no_link "Delete"
       end
     end
 
     context "ログインしているとき" do
       before do
         visit new_user_session_path
-        fill_in "Eメール", with: "#{@user.email}"
-        fill_in "パスワード", with: "#{@user.password}"
-        click_button "ログイン"
+        fill_in "Email", with: "#{@user.email}"
+        fill_in "Password", with: "#{@user.password}"
+        click_button "Log in"
         visit post_path(@post)
       end
 
       it "コメントできること" do
-        fill_in "コメント", with: "私もこの作品が好きです"
-        click_button "コメントする" 
+        fill_in "Content", with: "私もこの作品が好きです"
+        click_button "Comment" 
         expect(page).to have_content "私もこの作品が好きです"
       end
 
       it "削除できること" do
-        click_link "削除"
+        click_link "Delete"
         expect{
-          expect(page.accept_confirm).to eq "#{@comment.content}を削除します。よろしいですか？"
-          expect(page).to have_content "#{@comment.content}を削除しました"
+          expect(page.accept_confirm).to eq "#{@comment.content} will be deleted. Are you sure?"
+          expect(page).to have_content "#{@comment.content} deleted successfully."
           }.to change(@user.comments, :count).by(-1)
       end
 
       it "他のユーザーの投稿には削除リンクが出ていないこと" do
         visit post_path(@other_post)
-        expect(page).to have_no_link "削除"
+        expect(page).to have_no_link "Delete"
       end
     end
 
@@ -66,19 +66,19 @@ RSpec.describe "Comments", type: :system do
       before do
         @admin_user = FactoryBot.create(:admin_user)
         visit new_user_session_path
-        fill_in "Eメール", with: "#{@admin_user.email}"
-        fill_in "パスワード", with: "#{@admin_user.password}"
-        click_button "ログイン"
+        fill_in "Email", with: "#{@admin_user.email}"
+        fill_in "Password", with: "#{@admin_user.password}"
+        click_button "Log in"
       end
 
       it "他のユーザーのコメントも削除できること" do
         visit post_path(@other_post)
         within ".comment-delete" do
-          click_link "削除"
+          click_link "Delete"
         end
         expect{
-          expect(page.accept_confirm).to eq "#{@other_comment.content}を削除します。よろしいですか？"
-          expect(page).to have_content "#{@other_comment.content}を削除しました"
+          expect(page.accept_confirm).to eq "#{@other_comment.content} will be deleted. Are you sure?"
+          expect(page).to have_content "#{@other_comment.content} deleted successfully."
           }.to change(@other_user.comments, :count).by(-1)
       end
     end
